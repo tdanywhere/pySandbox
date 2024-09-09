1f5f6a2dd5b01bba7ba5618d6efb93877a0f1467
tdanywhere <thdeter@hotmail.com>
2024-09-09 22:33:24 +0200

    create Excel file from result and use yaml for sql queries

--- QueryData.py
+++ QueryData.py
@@ -11,27 +11,34 @@ from ConnectDb import OracleDB
 print("Start")
 
 # Load database connection info from YAML file
-with open('db_connection.yaml', 'r') as file:
+with open('./yaml/db_connection.yaml', 'r') as file:
     db_config = yaml.safe_load(file)
 
-# print("db_config: ", db_config['user'], db_config['password'], db_config['dsn'])
+# Load sql query from yaml 
+with open('./yaml/db_sqlQuery.yaml', 'r') as file:
+    sql_query = yaml.safe_load(file)
+
 
 # Database connection string
 db = OracleDB(username=db_config['user'], password=db_config['password'], dsn=db_config['dsn'])
 connection = db.getConnection()
 
 # Query the sandbox table
-query = "SELECT * FROM sandbox.sandbox"
+query = sql_query['sql'][sql_query['chosen_sql']]
 # print(query)
 
 # Read the result into a DataFrame
 df = pd.read_sql(query, con=connection)
  
+# Close the connection
+db.disconnect()
+
 # Display the result in a table
 print(df)
 
-# Close the connection
-db.disconnect()
+path_to_excel_file = './output/sandbox.xlsx'
+df.to_excel(path_to_excel_file, sheet_name='Sheet1', index=False)
+
 
 print("End")
 # End of file
